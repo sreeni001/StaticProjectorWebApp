7c3e34e16394d192cf18027e1450625fe276f540
sreeni001 <ashuradhagopalan@gmail.com>
2024-09-21 19:12:38 +0530

    Assets of LoginButton-Modified MainPage.js,SideBar js files- Created

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,10 @@
 import {React,useState} from 'react';
 import './cssfiles/login.css'
+import LoginButton from './assets/LoginButton.png'
 import logo from './assets/login.png';
 function Login(){
         //const [userName,getUsername] = useState('');
-        
+        const printVal=()=> console.log("Hi");
         return(
             <>
                 <div className='Container'>
@@ -14,6 +15,7 @@ function Login(){
                     <br/>
                     
                     <input type='password' name='pass' className='pass' placeholder='Password'/>
+                    <img src={LoginButton} onClick={printVal} className='LoginButton'/>
                 </div>
                 </div>
             </>
---
+++ src/components/MainPage.js
@@ -0,0 +1,10 @@
+import {React,useState} from 'react';
+
+const MainPage = ()=>{
+    
+    return(
+        <>
+        </>
+    );
+}
+export default MainPage;
